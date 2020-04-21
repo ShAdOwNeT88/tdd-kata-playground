a8933e22698667de5d53f6fcb3a879d88dea5b8d
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2020-04-21 18:54:05 +0200

    First iteration of String calculator excercise with set of tests

--- src/main/java/stringCalculator/StringCalculator.kt
+++ src/main/java/stringCalculator/StringCalculator.kt
@@ -2,9 +2,23 @@ package stringCalculator
 
 class StringCalculator {
 
-    fun add(numbers: String): String {
+    fun add(numbers: String): Int {
         return if (numbers == "")
-            "0"
-        else numbers
+            0
+        else {
+            var sum = 0
+            val obtainedNumbers = split(numbers)
+            obtainedNumbers.forEach { number ->
+                sum += number
+            }
+            return sum
+        }
+    }
+
+    fun split(numbers: String): List<Int> {
+        val convertedNumbers = ArrayList<Int>()
+        numbers.split(",").listIterator().forEach { number -> convertedNumbers.add(Integer.parseInt(number)) }
+
+        return convertedNumbers
     }
 }
--- src/test/kotlin/stringCalculator/StringCalculatorTest.kt
+++ src/test/kotlin/stringCalculator/StringCalculatorTest.kt
@@ -5,16 +5,43 @@ import org.junit.jupiter.api.Assertions.assertTrue
 
 class StringCalculatorTest {
 
+    private val sc: StringCalculator = StringCalculator()
+
     @Test
     fun `test if input is not null`() {
-        val actual = StringCalculator().add("")
-        val expected = "0"
+        val actual = sc.add("")
+        val expected = 0
         assertTrue(actual == expected)
     }
 
     @Test
     fun `test with input not null result is not 0`() {
-        val actual = StringCalculator().add("1")
-        assertTrue(!actual.contains("0"))
+        val actual = sc.add("1")
+        assertTrue(actual != 0)
+    }
+
+    @Test
+    fun `test if sum can handle all the numbers in input`(){
+        val actual = sc.add("1,2")
+        val expected = 3
+        assertTrue(actual == expected)
+    }
+
+    @Test
+    fun `test split with two numbers`(){
+        val actual = sc.split("1,2")
+        println("Actual Result$actual")
+        val expected = listOf(1,2)
+
+        assertTrue(actual == expected)
+    }
+
+    @Test
+    fun `test split with three numbers`(){
+        val actual = sc.split("1,2,3")
+        println("Actual Result$actual")
+        val expected = listOf(1,2,3)
+
+        assertTrue(actual == expected)
     }
 }
